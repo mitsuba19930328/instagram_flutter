0139bb02ef111dcedfbe85fd1d8cbd53d928bb55
rui-shimazaki <rui-shimazaki@cuebic.co.jp>
2022-03-14 00:54:46 +0900

    [ADD]Firebase初期設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyAx95xx7K61_GRFY7LFVfgUZynrw_OoyRc',
+        appId: '1:457137778462:web:820183d95e3992f959230f',
+        messagingSenderId: '457137778462',
+        projectId: 'instagram-tut-e9e28',
+        storageBucket: 'instagram-tut-e9e28.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
